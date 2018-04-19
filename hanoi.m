3e1d836d4cac425921d379363f9827f922957674
mlazaric <marko.lazaric@gmail.com>
2018-04-19 17:29:25 +0200

    Fresh start, different approach

--- hanoi.m
+++ hanoi.m
@@ -1,66 +1,19 @@
-function [G, p] = hanoi(numOfDisks, numOfPegs)
-    numOfBits = ceil(log2(numOfPegs));
-    offset = 2^numOfBits;
+function hanoi(numOfDisks, numOfPegs)
+    numOfVertices = numOfPegs^numOfDisks;
     
-    edges = zeros(numOfPegs^numOfDisks, 1);
-    names = cell(numOfPegs^numOfDisks, 1);
-    number_in_base = zeros(numOfDisks, 1);
-    curr_number = 0;
-    curr_name = repmat('A', 1, numOfDisks);
-    
-    for edge = 0:(numOfPegs^numOfDisks-1)
-        edges(edge + 1) = curr_number;
-        names{edge + 1} = curr_name;
-        
-        number_in_base(1) = number_in_base(1) + 1;
-        curr_number = 0;
-        curr_name = '';
-        
-        for digit = 1:(numOfDisks - 1)
-            if number_in_base(digit) == numOfPegs
-                number_in_base(digit + 1) = number_in_base(digit + 1) + 1;
-                number_in_base(digit) = 0;
-            end    
-        end
-        
-        for digit = 1:numOfDisks
-            curr_number = curr_number + number_in_base(digit) * offset^(digit - 1);
-            curr_name = strcat(char(65 + number_in_base(digit)), curr_name);
+    for from = 1:numOfVertices
+        for to = (from + 1):numOfVertices
+            % check if it is valid
         end
     end
     
-    [X, Y] = meshgrid(edges, edges);
-    A = arrayfun(@hanoiEdgeCheck, X, Y);
-    G = graph(A, names);
-    p = plot(G);
-
-
-    function isValidEdge = hanoiEdgeCheck(from, to)
-        isValidEdge = 1;
-
-        if from == to
-           isValidEdge = 0;
-        else
-            numOfChanges = 0;
-            changed = -1;
-
-            for disk=0:(numOfDisks - 1)
-                numOfChanges = numOfChanges + ((bitand(bitshift(from, -disk * numOfBits), offset - 1)) ~= (bitand(bitshift(to, -disk * numOfBits), offset - 1))); 
-
-                if numOfChanges == 1 && changed == -1
-                    changed = disk;
-                end
-            end
-
-            if numOfChanges > 1 
-                isValidEdge = 0;
-            else
-                for disk=0:(changed - 1)
-                    if (bitand(bitshift(from, -disk * numOfBits), offset - 1)) == (bitand(bitshift(from, -changed * 2), offset - 1)) || (bitand(bitshift(to, -disk * numOfBits), offset - 1)) == (bitand(bitshift(to, -changed * numOfBits), offset - 1))
-                       isValidEdge = 0;
-                    end
-                end
-            end
+    % Accepts an array of size numOfPegs that contains integers
+    % which represent the position of that particular disk
+    function name = getNameOfNode(positionsOfDisks)
+        name = '';
+        
+        for disk = 1:numOfPegs
+            name = strcat(name, char(positionsOfDisks(disk) + double('A')));
         end
     end
 end
